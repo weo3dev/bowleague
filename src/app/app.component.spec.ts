5319255e47c3209414db3a8435907d260984362b
William ONeal <metavurt@gmail.com>
2017-03-28 20:41:10 -0700

    Test run, clearing up references to deleted components.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,8 +4,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { LeadersComponent } from './leaders/leaders.component';
 import { TeamsComponent } from './teams/teams.component';
@@ -31,8 +29,6 @@ describe('AppComponent', () => {
       ],      
       declarations: [
         AppComponent,
-        HeaderComponent,
-        NavigationComponent,
         HomeComponent,
         LeadersComponent,
         TeamsComponent,
